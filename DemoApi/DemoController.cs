d3cf48fbc9e03750a6a333e66d5e216ce1c40f35
Deniz Doğru <deniz.dogru@venhancer.com>
2025-01-23 18:29:42 +0300

    minor

--- DemoApi/DemoController.cs
+++ DemoApi/DemoController.cs
@@ -4,14 +4,23 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 
-    [ApiController]
-    [Route("api/[controller]")]
-    public class DemoController : ControllerBase
+[ApiController]
+[Route("api/[controller]")]
+public class DemoController : ControllerBase
+{
+    [HttpGet("test")]
+    [Authorize(AuthenticationSchemes = "ApiKey")] // Require API key authentication
+    public IActionResult GetTest()
     {
-        [HttpGet("test")]
-        [Authorize(AuthenticationSchemes = "ApiKey")] // Require API key authentication
-        public IActionResult GetTest()
-        {
-            return Ok(new { Message = "Hello from Demo API!" });
-        }
+        return Ok(new { Message = "Hello from Demo API (API Key Authentication)!" });
     }
+
+}
+
+
+
+
+
+
+
+
